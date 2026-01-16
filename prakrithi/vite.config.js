2e6845241f4b7f149fe6235e186015969c7bc72e
aswinsagar12 <aswinsagar12@gmail.com>
2026-01-16 10:10:58 +0530

    chore: update package.json and vite.config.js for deployment
    - Added homepage URL to package.json for GitHub Pages
    - Included gh-pages dependency in package.json
    - Set base path in vite.config.js to match GitHub repository name

--- prakrithi/vite.config.js
+++ prakrithi/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/prakrithi-clothing/', // 👈 EXACT repo name
   plugins: [react()],
 })
