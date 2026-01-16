635042b9aa2c2655895d1b82547a67318fed417e
aswinsagar12 <aswinsagar12@gmail.com>
2026-01-16 10:25:50 +0530

    fix: update homepage URL and base path for Vite configuration

--- prakrithi/vite.config.js
+++ prakrithi/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/prakrithi-clothing/', // 👈 EXACT repo name
+  base: '/PrakrithiClothing/',
   plugins: [react()],
 })
